1796cbe7964a1e36af92c07a90f91ee7bceb2b85
Neel Shah <shah_neel@hotmail.com>
2017-05-23 15:15:50 +0100

    Added a Article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.populate 10 do |article|
+  title = Faker::StarWars.character
+  content = Faker::StarWars.quote
+end
